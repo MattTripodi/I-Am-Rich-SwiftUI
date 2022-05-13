b1b18de31fae50621c6192a45662c3f61470ae6b
Matthew Tripodi <mtripodi1027@gmail.com>
2022-05-13 09:51:17 -0400

    Initial Commit

---
+++ I Am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Matthew Tripodi on 5/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I Am Rich SwiftUI/I_Am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SwiftUIApp.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Matthew Tripodi on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
